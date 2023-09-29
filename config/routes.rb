67631fc76b8326694368f21d57c0c408c3bc0e89
Jamie Heo <heojiyun@gmail.com>
2023-09-29 19:23:50 +1000

    updated about me page desc

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+  get "about", to: "pages#about", as: "about"
+  get "projects", to: "pages#projects", as: "projects"
+  get "resume", to: "pages#resume", as: "resume"
 end
